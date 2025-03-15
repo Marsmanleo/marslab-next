5bca494fb831285afb46c26cdd0f0f200d1be2e5
leo <marstvam@gmail.com>
2025-03-16 02:27:05 +0800

    添加URL重写规则和图片文件

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 
 const nextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/data/image/:path*',
+        destination: 'http://localhost:8000/data/image/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
